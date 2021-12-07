66582abb05fdd0aaba39f09cf2d46487dcac67dd
miramonster <95644636+miramonster@users.noreply.github.com>
2021-12-07 14:48:49 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
-first_number = float (input("enter your first number: "))
-second_number = float (input("enter your second number: "))
-operation =   (input("enter your operation: "))
-
-def multiply(first_number, second_number):
-    print(f"Multiplication result: {first_number * second_number}")
-
-def divide(first_number, second_number):
-    print(f"division result: {first_number / second_number}")
-
-def addition(first_number, second_number):
-    print(f"Addition result: {first_number + second_number}")
-
-def subtraction(first_number, second_number):
-    print(f"Subtraction result: {first_number - second_number}")
-
-if operation == '*':
-    multiply(first_number, second_number)
-elif operation == '/':
-    divide(first_number, second_number)
-elif operation == '+':
-    addition(first_number, second_number)
-elif operation == '-':
-   subtraction(first_number, second_number)
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print(f"{num} is Even")
 else:
-    print("invalid request")
+   print(f"{num} is Odd")
